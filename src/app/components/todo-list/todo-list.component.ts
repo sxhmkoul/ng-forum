96955156423ca480545ea8460cd5e494cad328c7
Saksham Koul <sxhmkoul@gmail.com>
2023-03-12 01:27:20 +0530

    raw todo list

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent {
+  @ViewChild('inputList') inputList !: ElementRef;
+  todoListArray : string[] = [''];
+  doneListArray : string[] = [''];
+
+  onAddToList = () => {
+    this.todoListArray.push(this.inputList.nativeElement.value);
+    this.inputList.nativeElement.value = '';
+  }
+
+  onRemoveFromList = (event: any) => {
+    console.log(event);
+    event.target.classList.add('markedDone');
+    // this.todoListArray.splice(this.todoListArray.indexOf(event.target.textContent),this.todoListArray.indexOf(event.target.textContent) + 1);
+    this.doneListArray.push(event.target.textContent);
+  }
 
 }
