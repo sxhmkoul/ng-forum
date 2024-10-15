e802cc80d9d3b799463afb804eecaf1cca42fbc5
Saksham Koul <sxhmkoul@gmail.com>
2024-10-16 02:18:33 +0530

    deleted comment

--- src/app/components/menu-wrapper/menu-wrapper.component.ts
+++ src/app/components/menu-wrapper/menu-wrapper.component.ts
@@ -18,7 +18,6 @@ export class MenuWrapperComponent {
     { icon: 'fa-newspaper', text: 'Feed', routerLink: '/feed' },
     { icon: 'fa-clipboard', text: 'My Posts', routerLink: '/my-posts' },
     { icon: 'fa-heart', text: 'Liked', routerLink: '/liked' },
-    // { icon: 'fa-clipboard', text: 'Tags', routerLink: '/feed/tags' },
     { icon: 'fa-user', text: 'Profile', routerLink: '/profile' },
   ];
   test = '';
